b26d6c5503a8f5d9f3c884de15e625e50d435f31
Jaideep Kekre <jaideepkekre@gmail.com>
2019-07-24 13:16:12 +0530

    Added update ability

--- tester.go
+++ tester.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"./src/GODP"
+	"github.com/common-nighthawk/go-figure"
 )
 
 func main() {
-	// trailurl := "/api/c/" + appName + "/" + serviceName
 
 	odpOBJ := GODP.ODP{}
 	odpOBJ.BaseURL = "https://cloud.odp.capiot.com"
@@ -14,8 +14,9 @@ func main() {
 	odpOBJ.AppName = "Bajaj"
 	odpOBJ.ServiceName = "odptester"
 	odpOBJ.JWT = ""
+	figure.NewFigure("Go O.D.P !", "", true).Print()
 
-	println("odp object initialized\n")
+	println("\nodp object initialized\n")
 	var r = make(map[string]string)
 	r["count"] = "5"
 	r["filter"] = "{\"name\":\"Test\"}"
@@ -36,5 +37,24 @@ func main() {
 	println("Create Item started firing")
 	resp3, status3 := odpOBJ.CreateItem(testBodyMap)
 	println("Create Item fired with response code:", status3, " and created ODP object with id:", resp3["_id"].(string))
+
+	testBodyMap["name"] = "jaideep_updated"
+	println("Update Item started firing")
+	resp4, status4 := odpOBJ.UpdateItem(testBodyMap, resp3["_id"].(string))
+	println("Update Item fired with response code:", status4,
+		" and created updated ODP object with id:", resp4["_id"].(string),
+		"with new val ", resp4["name"].(string))
+
+	if status1 != 200 {
+		panic("Get Items Failed")
+	}
+
+	if status2 != 200 {
+		panic("Get Item Failed")
+	}
+
+	if status3 != 200 {
+		panic("Create Items Failed")
+	}
 
 }
